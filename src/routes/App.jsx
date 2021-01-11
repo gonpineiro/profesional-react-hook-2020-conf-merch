40ab951a64f4604738790db4d4771ebdb9ddc70a
Gonzalo <gon.pineiro@gmail.com>
2021-01-11 13:09:00 -0300

    Maquetacion

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from '../components/Layout';
 
 import {
   Home,
@@ -12,18 +13,16 @@ import {
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/checkout" component={Checkout} />
-      <Route
-        exact
-        path="/checkout/information"
-        component={Information}
-      />
-      <Route exact path="/checkout/payment" component={Payment} />
-      <Route exact path="/checkout/success" component={Success} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/information" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={Success} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
